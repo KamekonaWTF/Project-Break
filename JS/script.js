87a3efed1716ef605ba617a545cce02e04354125
Angel Fernandez <johndoe@example.com>
2023-12-21 16:11:25 +0100

    rutas relativas

--- JS/script.js
+++ JS/script.js
@@ -1,15 +1,15 @@
 function changeBg() {
     const images = [
-        'url("../assets/wallpapers/bg1.jpg")',
-        'url("../assets/wallpapers/bg2.jpg")',
-        'url("../assets/wallpapers/bg3.jpg")',
-        'url("../assets/wallpapers/bg4.jpg")',
-        'url("../assets/wallpapers/bg5.jpg")',
-        'url("../assets/wallpapers/bg6.jpg")',
-        'url("../assets/wallpapers/bg7.jpg")',
-        'url("../assets/wallpapers/bg8.jpg")',
-        'url("../assets/wallpapers/bg9.jpg")',
-        'url("../assets/wallpapers/bg0.jpg")',
+        'url("./assets/wallpapers/bg1.jpg")',
+        'url("./assets/wallpapers/bg2.jpg")',
+        'url("./assets/wallpapers/bg3.jpg")',
+        'url("./assets/wallpapers/bg4.jpg")',
+        'url("./assets/wallpapers/bg5.jpg")',
+        'url("./assets/wallpapers/bg6.jpg")',
+        'url("./assets/wallpapers/bg7.jpg")',
+        'url("./assets/wallpapers/bg8.jpg")',
+        'url("./assets/wallpapers/bg9.jpg")',
+        'url("./assets/wallpapers/bg0.jpg")',
     ]
 
     const bg = document.querySelector('.wallpaper')
